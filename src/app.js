b3f200908010f0e43426f382af12d8874cd4adb3
geisniwander <geisniwander@gmail.com>
2023-01-13 23:00:39 -0300

    feat: implementation of the route for user registration with data validation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import dayjs from "dayjs";
+import joi from "joi";
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 
@@ -22,8 +24,43 @@ try {
 /*------ROUTES--------*/
 
 app.post("/participants", async (req, res) => {
+  const user = req.body;
+
+  const userSchema = joi.object({
+    name: joi.string().required(),
+  });
+
+  const userValidation = userSchema.validate(user, { abortEarly: false });
+
+  if (userValidation.error) {
+    const errors = userValidation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
+
   try {
-  } catch (err) {}
+    const userExists = await db
+      .collection("participants")
+      .findOne({ name: user.name });
+
+    if (userExists)
+      return res.status(409).send("Esse usuário já está cadastrado!");
+
+    await db
+      .collection("participants")
+      .insertOne({ name: user.name, lastStatus: Date.now() });
+    await db.collection("messages").insertOne({
+      from: user.name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: dayjs().format("h:m:s"),
+    });
+
+    res.status(201).send("Cadastro efetuado!");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Um erro inesperado aconteceu no servidor!");
+  }
 });
 
 app.post("/messages", async (req, res) => {
@@ -47,5 +84,5 @@ app.get("/messages", async (req, res) => {
 });
 
 app.listen(5000, () => {
-  console.log("Server is litening on port 5000");
+  console.log("Server is listening on port 5000");
 });
